67cd8a26f156abccdd7c3661846d33b33bcb6b86
petko.lyutskanov <simona2000petko@abv.bg>
2023-02-13 13:22:48 +0200

    Refactor the LibraryEventsConsumerIntegrationTest

--- src/test/java/intg/com/example/libraryeventsconsumer/consumer/LibraryEventsConsumerIntegrationTest.java
+++ src/test/java/intg/com/example/libraryeventsconsumer/consumer/LibraryEventsConsumerIntegrationTest.java
@@ -31,9 +31,11 @@ import org.springframework.test.context.TestPropertySource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.isA;
@@ -43,7 +45,8 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest
 @EmbeddedKafka(topics = {"library-events", "library-events-RETRY", "library-events-DLT"}, partitions = 3)
 @TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}"
-        , "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}"})
+        , "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}"
+        , "retryListener.startup=false"})
 public class LibraryEventsConsumerIntegrationTest {
 
     @Autowired
@@ -77,9 +80,12 @@ public class LibraryEventsConsumerIntegrationTest {
 
     @BeforeEach
     void setUp() {
-        for (MessageListenerContainer messageListenerContainer : endpointRegistry.getListenerContainers()) {
-            ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
-        }
+        MessageListenerContainer container = endpointRegistry.getListenerContainers()
+                .stream().filter(messageListenerContainer ->
+                        Objects.equals(messageListenerContainer.getGroupId(), "library-events-listener-group"))
+                .collect(Collectors.toList()).get(0);
+
+        ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic());
     }
 
     @AfterEach
